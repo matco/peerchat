226fcf708f5f7f9fcb90f8259760f22d64e8efca
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2017-03-03 15:36:32 +0100

    Use new Javascript features and remove legacy code

--- server.node.js
+++ server.node.js
@@ -7,63 +7,12 @@ var peers = [];
 
 var PORT = process.env.PORT || 1337;
 
-if(!Function.isFunction) {
-	Function.isFunction = function(object) {
-		return {}.toString.call(object) === '[object Function]';
-	};
-}
 //prototype
-String.prototype.leftPad = function(length, pad) {
-	//clone string
-	var string = this + '';
-	while(string.length < length) {
-		string = pad + string;
-	}
-	return string;
-};
-Number.prototype.pad = function(length, pad) {
-	return this.toString().leftPad(length, pad || '0');
-};
-Array.objectFilter = function(properties) {
-	return function(object) {
-		for(var property in properties) {
-			if(properties.hasOwnProperty(property)) {
-				//if object property is a function, call it only if the asked value is not a function too
-				var object_value = Function.isFunction(object[property]) && !Function.isFunction(properties[property]) ? object[property].call(object) : object[property];
-				if(object_value !== properties[property]) {
-					return false;
-				}
-			}
-		}
-		return true;
-	};
-};
-Array.objectMap = function(property) {
-	return function(object) {
-		return Function.isFunction(object[property]) ? object[property].call(object) : object[property];
-	};
-};
 Array.prototype.removeElement = function(element) {
 	var index = this.indexOf(element);
 	if(index !== -1) {
 		this.splice(index, 1);
 	}
-};
-Array.prototype.find = function(callback, thisArgument) {
-	var i = 0, length = this.length;
-	for(; i < length; i++) {
-		var element = this[i];
-		if(callback.call(thisArgument, element, i, this)) {
-			return element;
-		}
-	}
-	return undefined;
-};
-Date.prototype.toDisplay = function() {
-	return this.getDate().pad(2) + '.' + (this.getMonth() + 1).pad(2) + '.' + this.getFullYear();
-};
-Date.prototype.toFullDisplay = function() {
-	return this.toDisplay() + ' ' + this.getHours().pad(2) + ':' + this.getMinutes().pad(2) + ':' + this.getSeconds().pad(2);
 };
 
 //create http server
@@ -121,7 +70,7 @@ function send_callback(error) {
 }
 
 websocket_server.on('connection', function(connection) {
-	console.log(new Date().toFullDisplay() + ' New peer connected');
+	console.log(new Date().toISOString() + ' New peer connected');
 
 	//add new peer to peers list
 	var peer = {
@@ -132,35 +81,29 @@ websocket_server.on('connection', function(connection) {
 	connection.on('message', function(message, flags) {
 		//process only text message
 		if(!flags.binary) {
-			console.log(new Date().toFullDisplay() + ' Message received ' + message);
+			console.log(new Date().toISOString() + ' Message received ' + message);
 			var content = JSON.parse(message);
 			switch(content.type) {
 				case 'connection' :
 					peer.user = content.user;
 					//find other peers
-					var other_peers = peers.filter(function(p) {
-						return p.connection !== connection;
-					});
+					var other_peers = peers.filter(p => p.connection !== connection);
 					//return peers list to peer
-					var response = {type : 'connection', users : other_peers.map(Array.objectMap('user'))};
+					var response = {type : 'connection', users : other_peers.map(p => p.user)};
 					connection.send(JSON.stringify(response));
 					//broadcast message to all other connected peers
-					other_peers.forEach(function(p) {
-						p.connection.send(message, send_callback);
-					});
+					other_peers.forEach(p => p.connection.send(message, send_callback));
 					break;
 				case 'call' :
 					//send message to recipient peer designated in the message
-					var recipient = peers.find(function(p) {
-						return p.user.id === content.recipient;
-					});
+					var recipient = peers.find(p => p.user.id === content.recipient);
 					recipient.connection.send(message, send_callback);
 			}
 		}
 	});
 
 	connection.on('close', function(code) {
-		console.log(new Date().toFullDisplay() + ' Peer disconnected with code ' + code);
+		console.log(new Date().toISOString() + ' Peer disconnected with code ' + code);
 		//remove it from peers list
 		peers.removeElement(peer);
 		//notify all others peers
